3bbac7418b22a4028d7a7b4d087336b3f5f18a2f
kyro <9013ms@gmail.com>
2025-09-24 09:52:12 +0530

    update firebase-message-sw.js

--- firebase-message-sw.js
+++ firebase-message-sw.js
@@ -1,4 +1,3 @@
-const analytics = getAnalytics(app);
 // File: firebase-messaging-sw.js
 
 // Firebase SDKs import karein
@@ -7,18 +6,18 @@ importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js")
 
 // Aapke web app ki Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBVLkg5-aTOIFxtfBB0hwvOXNOTb9ns3JU",
-    authDomain: "yukuprotocol01.firebaseapp.com",
-    projectId: "yukuprotocol01",
-    storageBucket: "yukuprotocol01.firebasestorage.app",
-    messagingSenderId: "1077444358098",
-    appId: "1:1077444358098:web:ca1fd1028ef6ef2762620b",
-    measurementId: "G-WHVQRZLFW0"
+  apiKey: "AIzaSyBVLkg5-aTOIFxtfBB0hwvOXNOTb9ns3JU",
+  authDomain: "yukuprotocol01.firebaseapp.com",
+  projectId: "yukuprotocol01",
+  storageBucket: "yukuprotocol01.firebasestorage.app",
+  messagingSenderId: "1077444358098",
+  appId: "1:1077444358098:web:ca1fded28ef6ef2762620b",
+  measurementId: "G-WHVQRZLFW0"
 };
 
 // Firebase ko initialize karein
 if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(firebaseConfig);
 }
 
 const messaging = firebase.messaging();
@@ -30,8 +29,8 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/yuku-icon.png' // Aap yahan apne logo ka path daal sakte hain
+    icon: '/yuku-icon.png' // Optional: You can put a path to your logo here
   };
-
+  
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
